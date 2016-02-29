c90149196789f4d3e0b8af19bd79109f4f74ca10
Bo Bergstrom <bo.bergstrom@gmail.com>
2016-02-29 16:26:04 -0700

    initial work on alert reporting

---
+++ StringReporter.js
@@ -0,0 +1,7 @@
+
+
+var user_typing = prompt("Type a single word here","Just one...")
+
+if(user_typing != null){
+  alert("Hey, no need to SHOUT!" + "Simply saying" + user_typing.toLowerCase  "would have been fine!")
+}
